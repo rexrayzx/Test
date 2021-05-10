c05509cc4ae88e0578121fd1602bc890c2d329ac
rexrayzx <77148120+rexrayzx@users.noreply.github.com>
2021-05-10 10:51:41 +0530

    UnknownHost

--- src/main/java/com/eezy/generics/BaseTest.java
+++ src/main/java/com/eezy/generics/BaseTest.java
@@ -14,6 +14,7 @@ import java.nio.file.Paths;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.apache.commons.codec.binary.Base64;
 import org.openqa.selenium.remote.DesiredCapabilities;
